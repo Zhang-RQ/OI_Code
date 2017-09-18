f1dbcb4644b20cec46fc65ef832010ab794b1208
Zhang-RQ <Zhang-RQ@users.noreply.github.com>
2017-09-18 16:39:45 +0800

    bzoj1088

--- BZOJ/1088.cpp
+++ BZOJ/1088.cpp
@@ -1,34 +1,28 @@
-#include<cstdio>
-#include<cmath>
-#include<algorithm>
-#include<cstring>
-#include<vector>
-#include<map>
-#include<queue>
-#include<stack>
-#include<bitset>
-using namespace std;
-int n,l2[10010];
-int ans=0;
-bool judge(bool x)
+#include <cstdio>
+#include <cstring>
+#define MAXN 10010
+int l1[MAXN],l2[MAXN],n;
+int check()
 {
-    int b[10010];
-    memset(b,0,sizeof(b));
-    if(x) b[1]=1;
-    b[2]=l2[2]-b[1];
-    for(int i=2;i<=n;i++) {b[i+1]=l2[i]-b[i]-b[i-1];}
-    for(int i=1;i<=n;i++) if(b[i]!=0&&b[i]!=1) return 0;
-    if(b[n+1]) return 0;
+    int i,j;
+    for(i=2;i<=n;i++)
+        l1[i+1]=l2[i]-l1[i]-l1[i-1];
+    if(l1[n+1]) return 0;
     return 1;
 }
 int main()
 {
-     scanf("%d",&n);
-     for(int i=1;i<=n;i++)
-         scanf("%d",&l2[i]);
-    for(int i=0;i<=l2[1];i++)
+    int i,j,cnt=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        scanf("%d",&l2[i]);
+    for(i=0;i<=l2[1];i++)
     {
-        if(judge(i))    ans++;
+        memset(l1,0,sizeof(l1));  
+        l1[1]=i;
+        l1[2]=l2[1]-i;
+        if(check()) cnt++;
     }
-    printf("%d\n",ans);
+    printf("%d\n",cnt);
+    return 0;
 }

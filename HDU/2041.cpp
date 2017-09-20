a67b6703845fd6fcc8296fd6d20c956949b03610
Zhang-RQ <Zhang-RQ@users.noreply.github.com>
2017-09-20 19:45:25 +0800

    hdu2044&2041

---
+++ HDU/2041.cpp
@@ -0,0 +1,24 @@
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<queue>
+#include<stack>
+#include<bitset>
+using namespace std;
+long long F[60];
+int main()
+{
+    F[1]=F[2]=1;
+    for(int i=3;i<=55;i++) F[i]=F[i-1]+F[i-2];
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int l;
+        scanf("%d",&l);
+        printf("%lld\n",F[l]);
+    }
+}

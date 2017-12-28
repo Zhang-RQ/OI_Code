a139e0e7420ee5db2297d636faa211a452328aba
Zhang-RQ <Zhang-RQ@users.noreply.github.com>
2017-12-28 21:29:15 +0800

    BZOJ2693

---
+++ BZOJ/2693.cpp
@@ -0,0 +1,67 @@
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int MAXP=10000010;
+const int P=1e8+9;
+ll prime[MAXP],vis[MAXP],siz;
+ll h[MAXP];
+ll sum[MAXP];
+void get()
+{
+    h[1]=1;
+    for(int i=2;i<MAXP-5;i++)
+    {
+        if(!vis[i])  {prime[++siz]=i;h[i]=i*(1ll-i)%P;}
+        for(int j=1;j<=siz&&prime[j]*i<MAXP-5;j++)
+        {
+            vis[prime[j]*i]=1;
+            if(!(i%prime[j]))
+                {h[i*prime[j]]=h[i]*prime[j]%P;break;}
+            else h[i*prime[j]]=h[i]*h[prime[j]]%P;
+        }
+    }
+    for(int i=1;i<MAXP-5;i++)
+        sum[i]=(sum[i-1]+h[i])%P;
+}
+ll Sum(ll x,ll y)
+{
+    x%=P;y%=P;
+    ll re1=(x*(x+1)>>1)%P;
+    ll re2=(y*(y+1)>>1)%P;
+    return (re1*re2)%P;
+}
+ll solve(ll n,ll m)
+{
+    if(n>m) swap(n,m);
+    int lim=min(n,m);
+    ll ans=0;
+    for(int d=1;d<=lim;)
+    {
+        int pos=min(n/(n/d),m/(m/d));
+        (ans+=(sum[pos]-sum[d-1])*Sum(n/d,m/d)%P)%=P;
+        d=pos+1;
+    }
+    return (ans+P)%P;
+}
+ll T,M,N;
+int main()
+{
+    get();
+    scanf("%lld",&T);
+    while(T--)
+    {
+        scanf("%lld%lld",&N,&M);
+        printf("%lld\n",solve(N,M)%P);
+    }
+}

dcc23c0ec836a0c478a8baec86a028fa3c61b10f
Zhang-RQ <a771902599@gmail.com>
2018-04-11 19:20:22 +0800

    POJ3683

--- POJ/3683.cpp
+++ POJ/3683.cpp
@@ -52,52 +52,55 @@ void tarjan(int x)
         bl[x]=SCC;ins[x]=0;top--;
     }
 }
-void output(int x)
-{
-    if(x<10) printf("0%d",x);
-    else printf("%d",x);
-}
-inline bool intersect(const int &l,const int &r,const int &L,const int &R){return ((l>=R)||(r<=L))^1;}
+inline bool intersect(const int &l,const int &r,const int &L,const int &R){return ((l<=L&&r>L)||(L<=l&&R>l));}
 int main()
 {
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    while(~scanf("%d",&n))
     {
-        scanf("%s%s%d",t1+1,t2+1,&a[i].len);
-        sscanf(t1+1,"%d:%d",&t,&tt);
-        a[i].l=t*60+tt;
-        sscanf(t2+1,"%d:%d",&t,&tt);
-        a[i].r=t*60+tt;
-    }
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=i+1;j<=n;j++)
-        {
-            if(intersect(a[i].l,a[i].l+a[i].len,a[j].l,a[j].l+a[j].len))
-                add(i,j+1010),add(j,i+1010);
-            if(intersect(a[i].l,a[i].l+a[i].len,a[j].r-a[j].len,a[j].r))
-                add(i,j),add(j+1010,i+1010);
-            if(intersect(a[i].r-a[i].len,a[i].r,a[j].l,a[j].l+a[j].len))
-                add(i+1010,j+1010),add(j,i);
-            if(intersect(a[i].r-a[i].len,a[i].r,a[j].r-a[j].len,a[j].r))
-                add(i+1010,j),add(j+1010,i);
-        }
-    }
-    for(int i=1;i<=n;i++)
-        if(!dfn[i]) tarjan(i);
-    for(int i=1;i<=n;i++)
-        if(!dfn[i]) tarjan(i+1010);
-    bool ok=1;
-    for(int i=1;i<=n;i++)
-        if(bl[i]==bl[i+1010]) ok=0;
-    if(ok)
-    {
-        puts("YES");
+        memset(head,0,sizeof head);
+        memset(dfn,0,sizeof dfn);
+        memset(low,0,sizeof low);
+        memset(bl,0,sizeof bl);
+        cnt_e=0;top=0;SCC=0;
         for(int i=1;i<=n;i++)
         {
-            if(bl[i]<bl[i+1010]) output(a[i].l/60),printf(":"),output(a[i].l%60),printf(" "),output((a[i].l+a[i].len)/60),printf(":"),output((a[i].l+a[i].len)%60),puts("");
-            else output((a[i].r-a[i].len)/60),printf(":"),output((a[i].r-a[i].len)%60),printf(" "),output(a[i].r/60),printf(":"),output(a[i].r%60),puts("");
+            scanf("%s%s%d",t1+1,t2+1,&a[i].len);
+            sscanf(t1+1,"%d:%d",&t,&tt);
+            a[i].l=t*60+tt;
+            sscanf(t2+1,"%d:%d",&t,&tt);
+            a[i].r=t*60+tt;
         }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j) continue;
+                if(intersect(a[i].l,a[i].l+a[i].len,a[j].l,a[j].l+a[j].len))
+                add(i,j+1010);
+                if(intersect(a[i].l,a[i].l+a[i].len,a[j].r-a[j].len,a[j].r))
+                add(i,j);
+                if(intersect(a[i].r-a[i].len,a[i].r,a[j].l,a[j].l+a[j].len))
+                add(i+1010,j+1010);
+                if(intersect(a[i].r-a[i].len,a[i].r,a[j].r-a[j].len,a[j].r))
+                add(i+1010,j);
+            }
+        }
+        for(int i=1;i<=n;i++)
+        if(!dfn[i]) tarjan(i);
+        for(int i=1;i<=n;i++)
+        if(!dfn[i+1010]) tarjan(i+1010);
+        bool ok=1;
+        for(int i=1;i<=n;i++)
+        if(bl[i]==bl[i+1010]) ok=0;
+        if(ok)
+        {
+            puts("YES");
+            for(int i=1;i<=n;i++)
+            {
+                if(bl[i]<bl[i+1010]) printf("%02d:%02d %02d:%02d\n",a[i].l/60,a[i].l%60,(a[i].l+a[i].len)/60,(a[i].l+a[i].len)%60);
+                else printf("%02d:%02d %02d:%02d\n",(a[i].r-a[i].len)/60,(a[i].r-a[i].len)%60,a[i].r/60,a[i].r%60);
+            }
+        }
+        else puts("NO");
     }
-    else puts("NO");
 }

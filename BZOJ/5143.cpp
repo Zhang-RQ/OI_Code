28318e041b747fc4331c8f5d5cbfc0aef0cf157e
Zhang-RQ <a771902599@gmail.com>
2018-09-05 21:04:46 +0800

    BZOJ 5143

---
+++ BZOJ/5143.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cassert>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+const int MAXN=1E5+10;
+const int BLKN=410;
+
+int a[MAXN],L[MAXN],R[MAXN],
+
+int main()
+{
+	
+}

8133f88b06d4081351dafbe7b9daa0e8f0bb90c1
Zhang-RQ <a771902599@gmail.com>
2018-10-11 10:51:06 +0800

    Fast_Read update

---
+++ 模板/Fast_Read.cpp
@@ -0,0 +1,26 @@
+namespace FastIO{
+    inline char get_char()
+    {
+        static char buf[1000001],*p1=buf,*p2=buf;
+        return p1==p2&&(p2=(p1=buf)+fread(buf,1,1000000,stdin),p1==p2)?EOF:*p1++;
+    }
+
+    template<typename T>
+        inline void read(T& x)
+        {
+            x=0;static char ch=get_char();
+            while(!isdigit(ch)) ch=get_char();
+            while(isdigit(ch)) x=x*10+(ch^'0'),ch=get_char();
+        }
+
+    #if __cplusplus >= 201103L
+        template<typename T,typename ...Args>
+            inline void read(T& x,Args& ...args)
+            {
+                read(x);
+                read(args...);
+            }
+    #endif
+}
+
+using namespace FastIO;

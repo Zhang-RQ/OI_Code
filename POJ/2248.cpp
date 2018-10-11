251ce894744c81dd8d6c204a35dfb1752dcabea9
Zhang-RQ <a771902599@gmail.com>
2018-10-11 10:49:34 +0800

    POJ 2248

--- POJ/2248.cpp
+++ POJ/2248.cpp
@@ -1,90 +1,56 @@
 #include<cstdio>
-#include<iostream>
-#include<cmath>
-#include<algorithm>
 #include<cstring>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
 #include<vector>
-#include<map>
 #include<set>
+#include<map>
 #include<queue>
 #include<stack>
-#include<bitset>
-using namespace std;
+#include<cassert>
+
 typedef long long ll;
-const int MAXN = 100 + 10;
+typedef unsigned long long ull;
 
-int num[MAXN], n, b[MAXN], ID;
+using namespace std;
 
-int dfs(int step)
+int n,Ans[410],tmp[410],Ans_t;
+int dep_lim;
+
+void dfs(int x)
 {
-    if(num[step - 1] == n) return 1;
-    if(step > ID)return 0;
-    register int tmp;
-    for(register int i = step - 1;i >= 1;-- i)
-        for(register int j = i;j >= 1;-- j)
+    if(x>dep_lim||Ans_t!=-1) return;
+    if((tmp[x]<<(dep_lim-x))<n) return;
+    if(tmp[x]==n)
+    {
+        Ans_t=x;
+        for(int i=1;i<=x;i++)
+            Ans[i]=tmp[i];
+        return;
+    }
+    for(int i=1;i<=x;i++)
         {
-            tmp = num[i] + num[j];
-            if(tmp <= num[step - 1] || b[tmp] || tmp > n)continue;
-            num[step] = tmp;
-            b[tmp] = 1;
-            if(dfs(step + 1)) return 1;
-            b[tmp] = 0;
+            tmp[x+1]=tmp[i]+tmp[x];
+            dfs(x+1);
         }
-    return 0;
 }
 
 int main()
 {
-    num[1] = 1;
-    while(scanf("%d", &n) != EOF && n)
-    {
-        for(register int i = 1;i <= n;++ i)
-        {
-            ID = i;
-            memset(b, 0, sizeof(b));
-            if(dfs(2))
-            {
-                for(register int j = 1;j <= i;++ j)
-                    printf("%d ", num[j]);
-                putchar('\n');
-                break;
-            }
-        }
-    }
-    return 0;
-}
-
-/*int arr[20];
-int n;
-int ans=20;
-int lg2[110];
-void dfs(int pos)
-{
-    if(lg2[(n/arr[pos-1])]+pos-1>ans) return;
-    if(pos>ans) return;
-    for(int i=1;i<pos;i++)
-    {
-        if(arr[i]+arr[1]>n) break;
-        for(int j=1;j<i;j++)
-        {
-            if(arr[i]+arr[j]>n) break;
-            if(arr[i]+arr[j]==n) {ans=min(ans,pos);return;}
-            arr[pos]=arr[i]+arr[j];
-            dfs(pos+1);
-        }
-    }
-    return ;
-}
-int main()
-{
-    for(int i=2;i<=109;i++) lg2[i]=lg2[i>>1]+1;
     while(~scanf("%d",&n)&&n)
     {
-        ans=20;
-        arr[1]=1;
-        arr[2]=2;
-        dfs(3);
-        printf("%d\n",ans);
+        Ans_t=-1;
+        for(int i=1;i<=n;i++)
+        {
+            dep_lim=i;
+            tmp[1]=1;dfs(1);
+            if(Ans_t!=-1) break;
+        }
+        for(int i=1;i<=Ans_t;i++)
+            printf("%d%c",Ans[i],i==Ans_t?'\n':' ');
     }
 }
-*/
+

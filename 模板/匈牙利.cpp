8ee89ae2c088e38c820755abe786c55d817cd55f
Zhang-RQ <a771902599@gmail.com>
2018-11-01 11:53:46 +0800

    匈牙利 update

--- 模板/匈牙利.cpp
+++ 模板/匈牙利.cpp
@@ -1,119 +1,54 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int maxn=1001;
-int n,m,v,tot,head[maxn*2],match[maxn],inCrossPath[maxn];
-struct near{
-    int to,nxt;
-}Edge[maxn*maxn];
-bool dfs(int x)
-{
-    int i=head[x];
-    while (i)
-    {
-        int p=Edge[i].to;
-        if (!inCrossPath[p])
-        {
-            inCrossPath[p]=1;
-            if (!match[p]||dfs(match[p]))
-            {
-                match[p]=x;
-                return 1;
-            }
-        }
-        i=Edge[i].nxt;
-    }
-    return 0;
-}
-int main()
-{
-    scanf("%d %d %d",&n,&m,&v);
-    int x,y;
-    for (int i=1;i<=v;i++)
-    {
-        scanf("%d %d",&x,&y);
-        if (x>n||y>m) continue;
-        Edge[++tot]=(near){y,head[x]};
-        head[x]=tot;
-    }
-    int ans=0;
-    for (int i=1;i<=n;i++)
-    {
-        memset(inCrossPath,0,sizeof(inCrossPath));
-        if (dfs(i)) ans++;
-    }
-    printf("%d\n",ans);
-    return 0;
-}
-
-
-/*#include<cstdio>
-#include<iostream>
-#include<cmath>
-#include<algorithm>
+#include<cstdio>
 #include<cstring>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
 #include<vector>
-#include<map>
 #include<set>
+#include<map>
 #include<queue>
 #include<stack>
-#include<bitset>
-using namespace std;
+#include<cassert>
+
 typedef long long ll;
 typedef unsigned long long ull;
-#define MAXM 1000010
-#define MAXN 1000100
-int head[MAXM<<1];
-struct Node{
-    int v,nxt;
-}Edge[MAXN<<1];
-int cnte=0;
-inline void add(const int u,const int &v)
+
+using namespace std;
+
+const int MAXN=1010;
+
+vector<int> g[MAXN];
+
+int n,m,e,mat[MAXN];
+bool inC[MAXN];  //inC mat 为右部点
+
+bool dfs(int x)
 {
-    Edge[++cnte].nxt=head[u];
-    Edge[cnte].v=v;
-    head[u]=cnte;
-}
-int n,m,e,u,v;
-bool inCrossPath[100020];
-int match[100200];
-bool findpath(int x)
-{
-    for(int i=head[x];i;i=Edge[i].nxt)
-    {
-        int vv=Edge[i].v;
-        if(!inCrossPath[vv])
+    for(int v:g[x])
+        if(!inC[v])
         {
-            inCrossPath[vv]=true;
-            if(match[vv]==-1||findpath(match[vv]))
-            {
-                match[vv]=u;
-                return true;
-            }
+            inC[v]=1;
+            if(!mat[v]||dfs(mat[v])) return mat[v]=x,1;
         }
-    }
-    return false;
+    return 0;
 }
-int hungary()
-{
-    int ret=0;
-    memset(match,-1,sizeof(match));
-    for(int i=1;i<=n;i++)
-    {
-        memset(inCrossPath,0,sizeof(inCrossPath));
-        if(findpath(i))
-            ret++;
-    }
-    return ret;
-}
+
 int main()
 {
     scanf("%d%d%d",&n,&m,&e);
-    for(int i=1;i<=e;i++)
+    for(int i=1,u,v;i<=e;i++)
     {
         scanf("%d%d",&u,&v);
-        if(v>m||u>n) continue;
-        v+=2000;
-        add(u,v);
+        if(u>n||v>m) continue;
+        g[u].push_back(v);
     }
-    printf("%d\n",hungary());
-}*/
+    int Ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        memset(inC,0,sizeof inC);
+        Ans+=dfs(i);
+    }
+    printf("%d\n",Ans);
+}
